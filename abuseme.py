6885d90d8c0e60d6e90747b24e8789fec9deb51d
Ananye <ananyesinghal@gmail.com>
2021-05-19 15:13:01 +0530

    added: new msg

--- abuseme.py
+++ abuseme.py
@@ -2,6 +2,6 @@ import random
 
 e=input("Enter name of the target: ")
 while e!='':
-	apologylst=["Sorry","I am really sorry...","I am good for nothing","It was my fault","Its all my fault","Please forgive me..."]
+	apologylst=["Sorry","I am really sorry...","I am good for nothing","It was my fault","Its all my fault","Please forgive me...", "I apologize"]
 	a=input("You: ")
 	print(e+':',random.choice(apologylst))
