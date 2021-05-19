5a71f2c01c424c782879ef205515af1e1c4c0648
Priyanshu Bhatt <priyanshubhatt80@gmail.com>
2021-05-19 16:08:49 +0530

    Revert "first"
    This reverts commit 3f50b32b0d1bf317dd778c10e744ca774d00a4ba.

--- abuseme.py
+++ abuseme.py
@@ -2,6 +2,6 @@ import random
 
 e=input("Enter name of the target: ")
 while e!='':
-	apologylst=["Sorry","I am really sorry...","I am good for nothing","It was my fault","Its all my fault","Please forgive me...", "I apologize"]
+	apologylst=["Sorry","I am really sorry...","I am good for nothing","It was my fault","Its all my fault","Please forgive me..."]
 	a=input("You: ")
 	print(e+':',random.choice(apologylst))
