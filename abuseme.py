3f50b32b0d1bf317dd778c10e744ca774d00a4ba
Priyanshu Bhatt <priyanshubhatt80@gmail.com>
2021-05-19 14:55:14 +0530

    first

---
+++ abuseme.py
@@ -0,0 +1,7 @@
+import random
+
+e=input("Enter name of the target: ")
+while e!='':
+	apologylst=["Sorry","I am really sorry...","I am good for nothing","It was my fault","Its all my fault","Please forgive me..."]
+	a=input("You: ")
+	print(e+':',random.choice(apologylst))
